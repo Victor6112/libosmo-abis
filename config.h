86a0871803e629b7de07cc5a1195c3a9099daafd
vic2 <50557899+Victor6112@users.noreply.github.com>
2020-01-15 02:54:54 +0300

    Add files via upload

---
+++ config.h
@@ -0,0 +1,71 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to 1 if you have the <dahdi/user.h> header file. */
+/* #undef HAVE_DAHDI_USER_H */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* ortp_set_log_level_mask has no domain parameter */
+#define HAVE_ORTP_LOG_DOMAIN 0
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory where libtool stores uninstalled libraries. */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "libosmo-abis"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "openbsc@lists.osmocom.org"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "libosmo-abis"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "libosmo-abis 0.5.1.20-de57"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libosmo-abis"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.5.1.20-de57"
+
+/* rtp_session_signal_connect requires ulong parameter */
+#define RTP_SIGNAL_PTR_CAST(arg) (unsigned long)(arg)
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Version number of package */
+#define VERSION "0.5.1.20-de57"
